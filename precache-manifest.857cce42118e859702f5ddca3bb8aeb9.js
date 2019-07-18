7cc283e239938add385493901b2cef314aff6288
Matta Viswa Jayanth <m.viswajayanth@gmail.com>
2019-07-18 21:22:11 +0530

    Updates

---
+++ precache-manifest.857cce42118e859702f5ddca3bb8aeb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17aabb04b54cb506d0d2013b2389a50",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "661ba6a38eea1b92b5bc",
+    "url": "/pics/static/css/main.ec4e0a22.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/pics/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "661ba6a38eea1b92b5bc",
+    "url": "/pics/static/js/main.92fde0b1.chunk.js"
+  },
+  {
+    "revision": "9198ae3b868f769fc8ea",
+    "url": "/pics/static/js/runtime~main.7218180c.js"
+  }
+]);
